cba5ff091d5ea39bdd434336dfb5635f0091eec4
obijuan <obijuan.cube@gmail.com>
2025-04-30 18:35:05 +0200

    Log: update example 5-btn0-led

--- wiki/Log/Ejemplos/05-btn0-led/main.s
+++ wiki/Log/Ejemplos/05-btn0-led/main.s
@@ -73,13 +73,12 @@ gpio_init:
 gpio_set_pulls:
     li	a0,0x40038004
     lw	a5,0(a0)
-    
+
     slli	a1,a1,0x3
     slli	a2,a2,0x2
     or	a2,a2,a1
     xor	a2,a2,a5
-    lui	a5,0x1
-    andi	a2,a2,12
-    add	a0,a0,a5
+    andi	a2,a2,0x0C 
+    li a0, 0x40039004
     sw	a2,0(a0)
     ret
