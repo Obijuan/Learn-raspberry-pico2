d235cfd3d519350139767f7339933dc94940fe07
obijuan <obijuan.cube@gmail.com>
2025-05-07 22:10:09 +0200

    log: update example 11-rxchar

--- wiki/Log/Ejemplos/11-rxchar/main.s
+++ wiki/Log/Ejemplos/11-rxchar/main.s
@@ -20,9 +20,6 @@ _start:
     #-- Configurar el LED
     jal led_init
 
-    #-- Configurar el pulsador
-    jal button_init15
-
     #-- Inicializar la UART
     #-- Velocidad: 115200 (con runtime actual)
     jal uart_init 
@@ -50,47 +47,5 @@ wait_rx:
     #-- Cambiar de estado el led
     jal led_toggle
     j main_loop 
-
-
-gpio_set_function:
-    
-    li a5, PAD_GPIO1
-    lw a4, 0(a5)
-
-    xori a4,a4, 0x40
-    andi a4,a4, 0xC0
-
-    li a3, PAD_GPIO1_XOR
-    sw a4, 0(a3)
-
-    li a0, GPIO1_CTRL
-    li a1, 2
-    sw a1, 0(a0)
-
-    li a5, PAD_GPIO1_CLR
-    li a4, 0x100
-    sw a4,0(a5)
-    ret
-
-
-
-    #-- Esperar a que el receptor tenga datos
-wait_rx2:
-    li t0, UART0_UARTF
-    lw t1, 0(t0)  
-    andi t1,t1,RXFF
-
-    #-- ¿Bit RXFF==1? (sin caracter), esperar
-    bne t1, zero, wait_rx2
-
-    #-- Leer dato
-    li t0,UART0_UARTDR
-    lw t1, 0(t0)
-
-    #-- Cambiar de estado el LED
-    jal led_toggle
-
-    #-- Repetir
-    j main_loop
 
 
