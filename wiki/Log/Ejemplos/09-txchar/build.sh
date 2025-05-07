4d97473e2ce18f289a8f48a9467b567e723813e2
obijuan <obijuan.cube@gmail.com>
2025-05-07 09:47:36 +0200

    Log:  Move  uart stuff to uart.s

--- wiki/Log/Ejemplos/09-txchar/build.sh
+++ wiki/Log/Ejemplos/09-txchar/build.sh
@@ -20,11 +20,12 @@ $AS -g -march=rv32i -mabi=ilp32 -I.. -o led.o led.s
 $AS -g -march=rv32i -mabi=ilp32 -I.. -o button.o button.s
 $AS -g -march=rv32i -mabi=ilp32 -I.. -o debug.o debug.s
 $AS -g -march=rv32i -mabi=ilp32 -I.. -o runtime.o runtime.s
+$AS -g -march=rv32i -mabi=ilp32 -I.. -o uart.o uart.s
 $AS -g -march=rv32i -mabi=ilp32 -I.. -o $MAIN.o $MAIN.s
 
 # -- Linkado
 $LD -g -m elf32lriscv -T linker.ld -o $MAIN.elf $MAIN.o  runtime.o \
-     led.o button.o debug.o  
+     led.o button.o debug.o uart.o 
 
 # -- Convertir a UF2
 $PICOTOOL uf2 convert $MAIN.elf $MAIN.uf2 --family rp2350-riscv --abs-block
