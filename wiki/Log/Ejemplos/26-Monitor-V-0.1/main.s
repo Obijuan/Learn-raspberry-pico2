a2a4e0eba4c4641b96f5dc250919bff45d81f885
obijuan <obijuan.cube@gmail.com>
2025-05-09 19:50:54 +0200

    log: update example 26

--- wiki/Log/Ejemplos/26-Monitor-V-0.1/main.s
+++ wiki/Log/Ejemplos/26-Monitor-V-0.1/main.s
@@ -5,8 +5,15 @@
 .section .rodata
 head1:  .ascii  "Monitor-V   0.1\n"
         .ascii  "──────────────────────────────────\n\0"
-info1:  .string "Comienzo flash:   "
-info2:  .string "Punto de entrada: "
+info1:  .string "Comienzo flash:          "
+info2:  .string "Punto de entrada:        "
+info3:  .string "Variables read-only:     "
+info4:  .string "Final del programa:      "
+info5:  .string "Variables inicializadas: "
+info6:  .string "Fin vars. inicializadas: "
+info7:  .string "Variables no inicializ.: "
+info8:  .string "Fin vars. no inicializ.: "
+info9:  .string "Puntero de pila:         "
 
 
 .section .text
@@ -57,6 +64,64 @@ main:
     jal print_0x_hex32
     li a0, '\n'
     jal putchar
+
+    #-- VARIABLES DE SOLO LECTURA
+    la a0, info3
+    jal print
+    la a0, __flash_ro_vars
+    jal print_0x_hex32
+    li a0, '\n'
+    jal putchar
+
+    #-- FINAL DEL PROGRAMA
+    la a0, info4
+    jal print
+    la a0, __flash_end
+    jal print_0x_hex32
+    li a0, '\n'
+    jal putchar
+
+    #-- VARIABLES INICIALIZADAS
+    la a0, info5
+    jal print
+    la a0, __data_ram_ini
+    jal print_0x_hex32
+    li a0, '\n'
+    jal putchar
+
+    #-- FIN VARIABLES INICIALIZADAS
+    la a0, info6
+    jal print
+    la a0, __data_ram_end
+    jal print_0x_hex32
+    li a0, '\n'
+    jal putchar
+
+    #-- VARIABLES NO INICIALIZADAS
+    la a0, info7
+    jal print
+    la a0, __var_no_init
+    jal print_0x_hex32
+    li a0, '\n'
+    jal putchar
+
+    #-- FIN DE LA RAM
+    la a0, info8
+    jal print
+    la a0, __data_end
+    jal print_0x_hex32
+    li a0, '\n'
+    jal putchar
+
+    #-- PILA
+    la a0, info9
+    jal print
+    la a0, _stack_top
+    jal print_0x_hex32
+    li a0, '\n'
+    jal putchar
+
+
 
     #-- Esperar a que se reciba un caracter
     jal getchar
