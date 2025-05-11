a20f5ce8b1ecfb54388ec3fbb2ab0436e96772b6
obijuan <obijuan.cube@gmail.com>
2025-05-11 11:03:16 +0200

    log: update example 27

--- wiki/Log/Ejemplos/27-Monitor-V-0.2/main.s
+++ wiki/Log/Ejemplos/27-Monitor-V-0.2/main.s
@@ -1,27 +1,10 @@
 
+.include "riscv.h"
 .include "boot.h"
 .include "regs.h"
 .include "uart.h"
 .include "ansi.h"
 
-#-----------------------------------------------
-#-- Macros de creación de funciones intermedias
-#-----------------------------------------------
-
-#-- Crear la pila con espacio para 4 palabras
-#-- en los offsets 12, 8, 4 y 0
-.macro FUNC_START4
-    addi sp, sp, 16
-    sw ra, 12(sp)
-.endm
-
-#-- Eliminar la pila de 4 palabras
-#-- y retornar
-.macro FUNC_END4
-    lw ra, 12(sp)
-    addi sp, sp, -16
-    ret
-.endm
 
 .section .rodata
 test:    .string "TESTING...\n"
