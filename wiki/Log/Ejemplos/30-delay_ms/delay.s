81f8c034cd553832e35edbbc60462d9cdfefab8d
obijuan <obijuan.cube@gmail.com>
2025-05-12 08:43:39 +0200

    log: update example 30

--- wiki/Log/Ejemplos/30-delay_ms/delay.s
+++ wiki/Log/Ejemplos/30-delay_ms/delay.s
@@ -1,6 +1,47 @@
+#-----------------------------------
+#-- FUNCIONES DE INTERFAZ
+#-----------------------------------
 .global delay
+.global delay_ms
+
+.include "regs.h"
 
 .section .text
+
+#---------------------------------------
+#-- delay_ms: Funcion de retardo, en ms
+#---------------------------------------
+#-- ENTRADAS:
+#--  a0: ms a esperar (>=1)
+#---------------------------------------
+delay_ms:
+
+    #-- Encender el AON-Timer
+    li t0, POWMAN_TIMER_SET
+    li t1, RUN
+    sw t1, 0(t0)
+
+    #-- Esperar hasta que transcurra el tiempo
+    #-- establecido
+
+_timer_wait:
+    
+    #-- Leer temporizador (en ms)
+    li t0, READ_TIME_LOWER
+    lw t1, 0(t0) 
+
+    #-- Ha transcurrido el tiempo?
+    bltu t1, a0, _timer_wait
+
+    #-- Ha pasado el tiempo indicado
+
+    #-- Apagar temporizador
+    li t0, POWMAN_TIMER_CLR
+    li t1, RUN
+    sw t1, 0(t0)
+
+    ret
+    
 
 # -----------------------------------------------
 # -- Delay
--- wiki/Log/Ejemplos/30-delay_ms/main.s
+++ wiki/Log/Ejemplos/30-delay_ms/main.s
@@ -7,6 +7,7 @@
 .include "regs.h"
 .include "uart.h"
 .include "ansi.h"
+.include "delay.h"
 
 .section .text
 
@@ -15,55 +16,21 @@ _start:
 
     #-- Acciones de arranque
     la sp, __stack_top
-    jal	runtime_init
 
+main:
     #-- Configurar perifericos
     jal led_init
-    jal uart_init
 
     #-- Encender el LED
     jal led_on
 
-main: 
+loop: 
 
-    li a0, 100
-    jal delay_ms
+    #-- Esperar
+    DELAY_MS 200
 
+    #-- Cambiar de estado el LED
     jal led_toggle
 
-    j main
-    
-#----------------------------------
-#-- Funcion de retardo, en ms
-#----------------------------------
-#-- ENTRADAS:
-#--  a0: ms a esperar (>=1)
-#----------------------------------
-delay_ms:
-
-    #-- Encender el AON-Timer
-    li t0, POWMAN_TIMER_SET
-    li t1, RUN
-    sw t1, 0(t0)
-
-    #-- Esperar hasta que transcurra el tiempo
-    #-- establecido
-
-_timer_wait:
-    
-    #-- Leer temporizador (en ms)
-    li t0, READ_TIME_LOWER
-    lw t1, 0(t0) 
-
-    #-- Ha transcurrido el tiempo?
-    bltu t1, a0, _timer_wait
-
-    #-- Ha pasado el tiempo indicado
-
-    #-- Apagar temporizador
-    li t0, POWMAN_TIMER_CLR
-    li t1, RUN
-    sw t1, 0(t0)
-
-    ret
+    j loop
     
