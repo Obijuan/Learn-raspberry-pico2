a67acb37602b29956cf186bae55f8512b4717d32
obijuan <obijuan.cube@gmail.com>
2025-05-15 09:57:52 +0200

    log: add example 32-traps

---
+++ wiki/Log/Ejemplos/32-traps/riscv.h
@@ -0,0 +1,18 @@
+#-----------------------------------------------
+#-- Macros de creación de funciones intermedias
+#-----------------------------------------------
+
+#-- Crear la pila con espacio para 4 palabras
+#-- en los offsets 12, 8, 4 y 0
+.macro FUNC_START4
+    addi sp, sp, 16
+    sw ra, 12(sp)
+.endm
+
+#-- Eliminar la pila de 4 palabras
+#-- y retornar
+.macro FUNC_END4
+    lw ra, 12(sp)
+    addi sp, sp, -16
+    ret
+.endm
