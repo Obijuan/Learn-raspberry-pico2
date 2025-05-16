720a6f414223b2564f4a37a643e120725d36a228
obijuan <obijuan.cube@gmail.com>
2025-05-16 23:54:38 +0200

    log: add example 38-led-u-mode

---
+++ wiki/Log/Ejemplos/38-led-u-mode/main.s
@@ -0,0 +1,77 @@
+#---------------------------
+#-- Funciones de interfaz
+#---------------------------
+.global _start   #-- Punto de entrada
+
+.include "riscv.h"
+.include "regs.h"
+.include "delay.h"
+.include "uart.h"
+.include "ansi.h"
+
+.section .text
+
+# -- Punto de entrada
+_start:
+
+    #-- Acciones de arranque
+    la sp, __stack_top
+    jal runtime_init
+
+    #-- Establecer el vector de interrupcion
+    la t0, isr
+    csrw mtvec, t0
+
+main:
+    #-- Configurar perifericos
+    jal led_init
+    jal uart_init
+
+    CLS
+
+    #-- Establecer permisos para acceder al LED
+    #-- desde el modo usuario
+    li t0, ACCESSCTRL_GPIO_NSMASK0
+    li t1, BIT25
+    sw t1, 0(t0) 
+
+    #-- Configurar los permisos de acceso a la flash
+    li t0, 0x5FFFFFFF
+    csrw pmpaddr0, t0
+
+    li t1, 0x1F
+    csrw pmpcfg0, t1
+
+    #------- PASAR A MODO USUARIO
+
+    #-- Poner a 0 bits del campo MPP
+    #-- Para que el modo de privilegio sea de usuario
+    csrw mstatus, zero
+
+    #-- Meter en mepc la direccion de retorno
+    la t0, user_mode_entry
+    csrw mepc, t0
+
+    #-- Saltar a modo usuario
+    mret 
+
+
+#------------------------------
+# Código en modo usuario
+#------------------------------
+user_mode_entry:
+
+    #-- Encender el LED desde 
+    #-- el modo usuario
+    jal led_on
+    j .
+
+#------------------------------------------
+#-- Rutina de atencion a la interrupcion 
+#------------------------------------------
+isr:
+    jal print_mstatus
+    jal print_mcause
+
+    jal led_blinky3
+    
