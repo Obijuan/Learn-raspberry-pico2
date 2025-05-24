9cba83cfa35cab18a6942cfd693252300ca9fb15
obijuan <obijuan.cube@gmail.com>
2025-05-24 20:44:31 +0200

    log: update example 39-debug-probe

--- wiki/Log/Ejemplos/39-debug-probe/exec.sh
+++ wiki/Log/Ejemplos/39-debug-probe/exec.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openocd -s tcl -f interface/cmsis-dap.cfg -f target/rp2350.cfg -c "adapter speed 5000" -c "program main.elf verify reset exit"
+openocd -s tcl -f interface/cmsis-dap.cfg -f target/rp2350-riscv.cfg -c "adapter speed 5000" -c "program main.elf verify reset exit"
