f8f65e70c53638bfae7b2184fb385759c439b7c8
obijuan <obijuan.cube@gmail.com>
2025-05-25 19:00:43 +0200

    log: update example 40-gdb-addi

--- wiki/Log/Ejemplos/40-gdb-addi/exec.sh
+++ wiki/Log/Ejemplos/40-gdb-addi/exec.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+#-- Script para programar la pico2 a través del depurador del DEBUG PROBE
+
 openocd -s tcl -f interface/cmsis-dap.cfg -f target/rp2350-riscv.cfg -c "adapter speed 5000" -c "program main.elf verify reset exit"
